72727ce8bfc69cb3c23001b21cadf68179778baf
mingkaic <mingkaic@sfu.ca>
2014-10-24 21:22:02 -0700

    Initial Login and signup

---
+++ SleepEzLogin/SleepEzLogin/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SleepEzLogin
+//
+//  Created by Ming Kai Chen on 2014-10-24.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
